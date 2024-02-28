ae35cb77bb951fd886935810c624ca727b1ad4f3
kamal0474 <140437394+kamal0474@users.noreply.github.com>
2024-02-28 14:56:20 +0000

    conversion of data

---
+++ 1_basic/conversionop.js
@@ -0,0 +1,13 @@
+let score = "abc" //99 don't no ki number hai ki string
+console.log(typeof score); //=>console.log(typeof (score));
+let value = Number(score)//by writing Number it's confirmed ki ye number hi hoga agar string krna hai to String likh do.
+console.log(typeof value);
+console.log(value);//printing statement
+let login = 1
+let boolean = Boolean(login)
+console.log(boolean);
+//1 => true, 0=>false, ""=>false
+let no =47
+let stri = String(no)
+console.log(typeof stri)
+console.log(stri);
