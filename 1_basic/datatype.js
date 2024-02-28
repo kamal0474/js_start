ca63af8e67f20c13b87f1f16b70f34ed54dac837
kamal0474 <140437394+kamal0474@users.noreply.github.com>
2024-02-28 11:47:06 +0000

    config done

---
+++ 1_basic/datatype.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) we are using nodejs, not browser
+// code reuseability should be high
+// datatypes
+// number => 2 to power 54
+//bigint 
+//string=>""
+//boolean=> true/false
+//null (stand alone value)
+//undefined => no value assigned
+// symbol=> for uniqueness
+// object 
+let name = "kamlu"
+let age = 20
+console.log(typeof "kamlu");
+console.log(typeof age);
+console.log(typeof null); //object 
+console.log(typeof undefined); //undefined
---
+++ 1_basic/var.js
@@ -0,0 +1,11 @@
+const account_Id = 12345
+let accountEmail = "kamli@google.com"
+var account_pass = "2147"
+account_city ="delhi"
+// account_Id = 223344
+accountEmail = "kd@gmail.com"
+account_pass = "21047"
+account_city = "dili"
+//console.log(account_Id);
+//console.log([account_Id, accountEmail, account_pass, account_city])
+console.table([account_Id, accountEmail, account_pass, account_city])
