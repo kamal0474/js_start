0d44baf49930294e07e39fc860345a67d359d767
kamal0474 <140437394+kamal0474@users.noreply.github.com>
2024-02-28 15:14:13 +0000

    operations

---
+++ 1_basic/operation.js
@@ -0,0 +1,12 @@
+let val = 4
+let negval = -val
+//console.log(negval);
+let name1 = "hello"
+let name2 =" cutiee"
+let fname = name1 + name2 
+console.log(fname);
+console.log("1" + 2);//12
+console.log(1 + "2");//12
+console.log("1" + 2 +2);//122
+console.log(1 + 2 +"2");//32 don't write all these of code in js
+console.log(+true); //don't write
