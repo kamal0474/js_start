6761b03d5ea4ced1a7326ee340935df1c0bb7114
kamal0474 <140437394+kamal0474@users.noreply.github.com>
2024-02-29 10:49:40 +0000

    numbers and maths

---
+++ 1_basic/num_math.js
@@ -0,0 +1,22 @@
+//****************numbers
+const score = 100
+//console.log(score);
+const newsc = new Number(100)
+//console.log(newsc)
+//console.log(newsc.toString().length)
+//console.log(newsc.toFixed(2))
+const newnum = 155.856
+//console.log(newnum.toPrecision(3))
+const newnum2 = 1000000
+//console.log(newnum2.toLocaleString('en-IN'))// isse comman mein aa jati hai value koi bhi hundred vali.
+//****************maths
+console.log(Math.abs(-4))// isse neg value pos mein aa jati hai.
+console.log(Math.round(4.6))// round off hoga
+console.log(Math.ceil(4.2))// top value choose krta hai
+console.log(Math.floor(4.3))// lower value choose krta hai.
+console.log(Math.min(3,5,6,1,8))// min value dega.
+console.log(Math.random()) // random ki value 0 se 1 ke beech mein hi aaegi
+console.log((Math.random()*10)+1)
+const min=10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min +1)) + min)//kitne se kitne ke range mein chaiye vo value ko store krne ke liye formula.
